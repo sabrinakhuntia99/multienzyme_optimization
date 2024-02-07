5e08ee0ea9db6b1f9cfe01e08d1b67911b55e3eb
Sabrina Khuntia <96092401+sabrinakhuntia99@users.noreply.github.com>
2024-02-07 17:11:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
+from __future__ import division
+from Bio.PDB import *
+from data_extraction import extract_aa_sequence, three_to_one
+from proteo_sim import cleave_with_enzyme
 
+p = PDBParser()
 
-# Load proteome data
-proteome_sequences = load_proteome_data()
-def simulate_sequential_cleavage(protein_sequence, enzyme):
-    cleaved_peptides = []
-    # Find enzyme recognition sites and cleave the sequence
-    # Append cleaved peptides to cleaved_peptides array
-    return cleaved_peptides
-def mass_spec_filter(cleaved_peptides):
-    # Filter peptides based on length (7-30 amino acids)
-    # Return peptides satisfying detectability conditions
-    return filtered_peptides
+#  AlphaFold structure for test
+structure = p.get_structure('O60361',
+                            r"C:\Users\Sabrina\PycharmProjects\structural_proteomics\venv\AF-O60361-F1-model_v4.pdb")
+
+# Extract one-letter AA sequence
+aa_sequence =(extract_aa_sequence(structure))
+sequence = three_to_one(aa_sequence)
+print(sequence)
 
 # Define protease cleavage sites
 enzymes = {
-    #"Trypsin": "cleavage after K or R",
-    #"Lys-C": "cleavage after K"
+    "Trypsin": "KR",
+    "Lys-C": "K"
 }
 
-results = {}
+# Choose the enzyme
+enzyme_name = "Trypsin"
+enzyme_cleavage_site = enzymes[enzyme_name]
 
-for enzyme_name, enzyme_pattern in enzymes.items():
-    detectable_peptides = []
-    detected_proteins = set()
+# Perform cleavage
+cleaved_peptides = cleave_with_enzyme(sequence, enzyme_cleavage_site)
 
-    # Simulate enzyme cleavage for each protein in the proteome
-    for protein_sequence in proteome_sequences:
-        cleaved_peptides = simulate_sequential_cleavage(protein_sequence, enzyme_pattern)
-        detectable_peptides.extend(mass_spec_filter(filtered_peptides))
-        detectable_proteins.add(protein_sequence)
-
-    # Calculate metrics for the current enzyme
-    results[enzyme_name] = {
-        "Number of detectable peptides": len(detectable_peptides),
-        "Number of detectable proteins": len(detectable_proteins),
-        # Peptide length distributi
-    }
-
-# Output results
+# Print the cleaved peptides
+print("Cleaved peptides using", enzyme_name + ":")
+for peptide in cleaved_peptides:
+    print(peptide)
